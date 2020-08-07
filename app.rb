e9d1c535ebe733dd2aef4694eb5e9cb859c47a05
Tomas Perez <tomasitopg6@gmail.com>
2020-08-07 05:42:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/' do
+  post '/puppy' do
     @new_pooch = Puppy.new(params[:name], params[:breed], params[:age])
 
     erb :display_puppy
